6fe2f990b2a7465c11fe1e84269af1f3f721c8a8
mpsolares <marianasolares@gmail.com>
2022-08-17 22:54:32 -0300

    Segunda Pre-entrega proyecto Final

--- js/main.js
+++ js/main.js
@@ -1,38 +1,83 @@
-const cursos = [
-    {id:01, name:"Curso de Aleman", img: "curso01.jpg", price:2000},
-    {id:02, name:"Curso de Español", img: "curso02.jpg", price:1000},
-    {id:03, name:"Curso de Ingles", img: "curso03.jpg", price:1500},
-    {id:04, name:"Curso de Italiano", img: "curso04.jpg", price:2500}
+// Creo el Array de Productos
+const pictures = [
+    {id:01, name:"Cuadro Acuarelas Marrones", img: "cuadro01.jpg", price:4000},
+    {id:02, name:"Cuadro Acuarelas Pasteles", img: "cuadro02.jpg", price:3000},
+    {id:03, name:"Combo dos Cuadros Acuarelas", img: "cuadro03.jpg", price:7500},
+    {id:04, name:"Cuadro Flores Acuarelas", img: "cuadro04.jpg", price:2500},
+    {id:05, name:"Cuadro Acuarelas Marrones", img: "cuadro04.jpg", price:4000},
+    {id:06, name:"Cuadro Acuarelas Pasteles", img: "cuadro03.jpg", price:3000},
+    {id:07, name:"Combo dos Cuadros Acuarelas", img: "cuadro02.jpg", price:7500},
+    {id:08, name:"Cuadro Flores Acuarelas", img: "cuadro01.jpg", price:2500},
+    {id:09, name:"Cuadro Acuarelas Marrones", img: "cuadro01.jpg", price:4000},
+    {id:10, name:"Cuadro Acuarelas Pasteles", img: "cuadro02.jpg", price:3000},
+    {id:11, name:"Combo dos Cuadros Acuarelas", img: "cuadro04.jpg", price:7500},
+    {id:12, name:"Cuadro Flores Acuarelas", img: "cuadro03.jpg", price:2500},
+    {id:13, name:"Cuadro Acuarelas Marrones", img: "cuadro03.jpg", price:4000},
+    {id:14, name:"Cuadro Acuarelas Pasteles", img: "cuadro02.jpg", price:3000},
+    {id:15, name:"Combo dos Cuadros Acuarelas", img: "cuadro01.jpg", price:7500},
+    {id:16, name:"Cuadro Flores Acuarelas", img: "cuadro04.jpg", price:2500}
 ];
 
-for (const curso of cursos) {
-    let container = document.createElement("div");
-    container.className = "col-md-3";
-    container.innerHTML = `<h3>ID ${curso.id}</h3>
-                            <p>${curso.name}<br>
-                            <img src="img/${curso.img}" width="180"><br>
-                            <b>$ ${curso.price}</b></p>
-                            <div class="card__btn">
-                            <button class="btn btn btn-success" data-product-sku="${curso.name}">Agregar</button>
-                            </div>      
-                            <hr>`;
-    document.getElementById("result").appendChild(container);
+// Funciones LocalStorage
+function savePicturesLS(pictures){
+  localStorage.setItem("cuadros", JSON.stringify(pictures));
 }
 
-let buttons = document.getElementsByTagName("button");
+function loadPictureslS(){
+  return JSON.parse(localStorage.getItem("Cuadros")) || [];
+}
 
-const cart = [];
+function savePicturesCart(pictures){
+  localStorage.setItem("cuadros_cart", JSON.stringify(pictures));
+}
 
-for (var i = 0; i < buttons.length; i++) {
-  let button = buttons[i];
-  console.log(button);
-  button.addEventListener('click', function(event){
-    console.clear();
-    console.log(event.target);
-    console.log(event.target.dataset.productSku);
-    cart.push( event.target.dataset.productSku );
-    console.log(cart)    
-  });
+function loadPicturesCart(){
+  return JSON.parse(localStorage.getItem("cuadros_cart")) || [];
+}
+7*
+function findPicture(id){
+  const pictures = loadPictureslS();
   
+  return pictures.find(item => item.id === id);
 }
 
+function addPicture(id){
+  const pictures_cart = loadPicturesCart();
+  const picture = findPicture(id);
+  pictures_cart.push(picture);
+  savePicturesCart(pictures_cart);
+}
+
+function btnCarritoLoad(){
+  const pictures_cart = loadPicturesCart();
+  let total = pictures_cart.length;
+  let cartContent = `<button type="button" class="btn position-relative mt-2" style="color:#778899;margin-right: 30px;">
+                      <iconify-icon icon="akar-icons:cart" height="30"></iconify-icon>  
+                      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary">${total}</span>
+                    </button>`;
+  document.getElementById("btn-cart").innerHTML = cartContent;
+}
+
+// Creo la Galeria de Productos y la almaceno con el onclick (usando id) en funcion addPicture
+function containerPictures(){
+  const pictures = loadPictureslS();
+  let gallery = "";
+
+  pictures.forEach((picture)=>{
+    gallery += `<div class="col-md-3">
+                <div class="card my-2 mb-3">
+                  <img src="img/${picture.img}" class="card-img-top" alt="${picture.name}">
+                  <div class="card-body">
+                    <p class="card-title">${picture.name}</p>
+                    <h5><b>$${picture.price}</b></h5>
+                    <a href="#" class="btn btn-outline-secondary align-self-center" onclick="addPicture(${picture.id})">Agregar <iconify-icon icon="akar-icons:cart" class="btn-bd-primary"></iconify-icon></a>
+                  </div>
+                </div>
+                </div>`;
+      document.getElementById("result").innerHTML = gallery;
+  }); 
+}
+
+savePicturesLS(pictures);
+containerPictures();
+btnCarritoLoad();
